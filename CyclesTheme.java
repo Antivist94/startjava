9adff92358c05b9d24ea9e7131732325dbb35150
Eugene Evdokimenko <antivist94@yandex.ru>
2023-04-10 23:01:35 +0400

    пуш от 10.04.23, ДЗ по циклам

--- CyclesTheme.java
+++ CyclesTheme.java
@@ -63,7 +63,7 @@ public class CyclesTheme {
         }
 
         System.out.println("\n5. Проверка количества двоек на четность/нечетность.");
-        int anyNum = 3242592;
+        anyNum = 3242592;
         int srcNum = 0;
         int countNumberTwo = 0;
         int showNum = anyNum;
@@ -91,14 +91,97 @@ public class CyclesTheme {
         while (width != 0) {
             int charPrint = 0;
             while (charPrint != width) {
-                 System.out.print("#");
+                 System.out.print('#');
                  charPrint++;
             }
         System.out.println();
         --width;
         }
+        System.out.println();
+        width = 0;
+        boolean increasing = true;
+        do {
+           for (int j = 0; j < width; j++) {
+              System.out.print('$');
+           }
+           System.out.println();
+           if (width == 3) {
+              increasing = false;
+           }
+           if (increasing) {
+                width++;
+           } else {
+                width--;
+           }
+        } while (width > 0);
 
+        System.out.println("\n7. Отображение ASCII-символов.");
+        System.out.print("Dec\t" + "Char");
+        for (int i = 1; i < 47; i+=2)  {
+           System.out.printf("\n%3d%6c",i ,(char) i);
+        }
+        for (int j = 98; j < 122; j+=2) {
+        System.out.printf("\n%3d%6c",j ,(char) j);
+        }
 
+        System.out.println("\n\n8. Проверка, является ли число палиндромом.");
+        srcNum = 1234321;
+        int polyNum = srcNum;
+        int backwardNum = 0;
+        while (srcNum > 0) {
+            anyNum = srcNum % 10;
+            srcNum /= 10;
+            backwardNum = backwardNum * 10 + anyNum;
+        } if (polyNum == backwardNum) {
+            System.out.println("Число " + polyNum + " - палиндром.");
+        } else {
+            System.out.println("Число " + polyNum + " не является палиндромом.");
+        }
 
+        System.out.println("\n9. Определение, является ли число счастливым.");
+        srcNum = 321123;
+        int counNumbers = srcNum; 
+        int firstHalf = 0;
+        int secondHalf = 0;
+        int counter = 0;
+        while (counNumbers > 0) {
+            counNumbers /= 10;
+            ++counter;
+        }
+        counter /= 2;
+        System.out.print("Число " + srcNum);
+        for (int i = 0; i < counter; i++) {
+            anyNum = srcNum % 10;
+            srcNum /= 10;
+            firstHalf += anyNum;
+        }
+        for (int i = 0; i < counter; i++) {
+            anyNum = srcNum % 10;
+            srcNum /= 10;
+            secondHalf += anyNum;
+        }
+        if (firstHalf == secondHalf) {
+            System.out.print(" является счастливым.\n");
+        } else {
+            System.out.print(" не является счастливым.\n");
+        }
+
+        System.out.println("\n10. Вывод таблицы умножения Пифагора.");
+        int j = 0;
+        System.out.printf("%3c", '|');
+        for ( int i = 2; i < 10; i++) {
+            System.out.printf("%3d", i);
+        }
+        System.out.println("\n---------------------------");
+        for (j = 2; j < 10; j++) {
+            if (j >= 3) {
+                System.out.println();
+            }
+            System.out.print(" " + j + "|");
+            for (int x = 2; x < 10; x++) {
+                int multiNum = x * j;
+                System.out.printf("%3d", multiNum);
+            }
+        } 
     }
 }
