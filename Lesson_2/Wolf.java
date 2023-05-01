c2a369247cff41fc25cc24a9e00eb08d674fa69d
Eugene Evdokimenko <antivist94@yandex.ru>
2023-05-01 17:17:23 +0400

    пуш от 01.05.23 Доработка

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -24,4 +24,44 @@ public class Wolf {
     public String hunt() {
         return "охотится";
     }
+
+    public String getSex() {
+            return sex;
+    }
+    
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,6 +1,14 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
+        wolf.setName("Тигр");
+        wolf.setSex("Самец");
+        wolf.setWeight(15);
+        wolf.setAge(4);
+        wolf.setColor("Чёрный");
+        System.out.println("Волка зовут: " + wolf.getName() + ". Пол: " + wolf.getSex() + "\n" 
+            + "Возраст: " + wolf.getAge() + " Вес: " + wolf.getWeight() + "\n" 
+            + "Цвет: " + wolf.getColor());
         System.out.println("Волк " +  wolf.move());
         System.out.println("Волк " +  wolf.seat());
         System.out.println("Волк " +  wolf.run());
